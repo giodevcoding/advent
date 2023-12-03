8b38ab4620fd2f93440ea7ef15a549419e69a6ab
Giovanni Panzetta <giovanni.panzetta@daveramsey.com>
2023-12-03 17:36:34 -0600

    most of the building blocks for 3.1, still need to get valid PartNumbers

--- 2023/three/gear_ratios.go
+++ 2023/three/gear_ratios.go
@@ -2,21 +2,108 @@ package three
 
 import (
 	"fmt"
+	"strconv"
+	"unicode"
+)
+
+type (
+	PartNumber struct {
+		value  int
+		index  int
+		length int
+	}
+	PartNumberSlice []PartNumber
 )
 
 func RunPartOne(input []string) {
-    IsSymbol("5")
-    IsSymbol('.')
+    
 }
 
-func IsSymbol[T rune | string](input T) bool {
-    inputType := fmt.Sprintf("%T", input)
-    
-    if inputType == "string" {
-
+func EngineSchematicSum(input []string) (sum int) {
+    partNumbers, symbols := GetPartNumbersAndSymbols(input) 
+    for y, line := range partNumbers {
+        for _, partNumber := range line {
+           if IsPartNumberValid (partNumber, y, symbols) {
+               sum += partNumber.value
+           }
+        }
     }
 
-    fmt.Println(inputType)
+    return sum
+}
 
+func IsPartNumberValid(partNumber PartNumber, partNumberY int, symbols [][]int) bool {
+    // TODO: Check if symbols contains an index that would make the PartNumber valid
+    return false
+}
+
+func GetPartNumbersAndSymbols(input []string) (partNumbers [][]PartNumber, symbols [][]int) {
+    for _, line := range input {
+        partNumbers = append(partNumbers, ExtractPartNumbersFromLine(line))
+        symbols = append(symbols, ExtractSymbolIndexesFromLine(line))
+    } 
+
+    return partNumbers, symbols
+}
+
+func ExtractPartNumbersFromLine(line string) PartNumberSlice {
+	partNumbers := []PartNumber{}
+	currentNumber := ""
+	currentIndex := 0
+
+    for i := 0; i < len(line); i++ {
+        char := rune(line[i])
+		if unicode.IsDigit(char) {
+			currentIndex = i
+            fmt.Println(currentIndex)
+			for j := i; j < len(line); j++ {
+				subChar := rune(line[j])
+
+				if !unicode.IsDigit(subChar) {
+					value, _ := strconv.Atoi(currentNumber)
+					partNumber := PartNumber{
+						value:  value,
+						index:  currentIndex,
+						length: len(currentNumber),
+					}
+
+					partNumbers = append(partNumbers, partNumber)
+
+					i = j
+					break
+				}
+
+				currentNumber += string(subChar)
+			}
+		}
+	}
+
+	return partNumbers
+}
+
+func ExtractSymbolIndexesFromLine(line string) []int {
+	indexes := []int{}
+	for i, char := range line {
+		if IsSymbol(char) {
+			indexes = append(indexes, i)
+		}
+	}
+	return indexes
+}
+
+func IsSymbol(input rune) bool {
+	return input != '.' && !unicode.IsDigit(input)
+}
+
+func (slice PartNumberSlice) Equals(otherSlice PartNumberSlice) bool {
+	if len(slice) != len(otherSlice) {
+		return false
+	}
+
+	for i := range slice {
+		if slice[i] != otherSlice[i] {
+            return false
+		}
+	}
     return true
 }
--- 2023/three/gear_ratios_test.go
+++ 2023/three/gear_ratios_test.go
@@ -1,7 +1,107 @@
 package three
 
-import "testing"
+import (
+	"advent/helpers"
+	"fmt"
+	"testing"
+)
 
 func TestEngineSchematicSum(t *testing.T) {
+	test := struct {
+		input    []string
+		expected int
+	}{
+		[]string{
+			"467..114..",
+			"...*......",
+			"..35..633.",
+			"......#...",
+			"617*......",
+			".....+.58.",
+			"..592.....",
+			"......755.",
+			"...$.*....",
+			".664.598..",
+		},
+		4361,
+	}
 
+    result := EngineSchematicSum(test.input)
+    if (result != test.expected) {
+        t.Errorf("EngineSchematicSum(%T) = %d; expected %d", test.input, result, test.expected)
+    }
+}
+
+func TestExtractSymbolIndexesFromLIne(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected []int
+	}{
+		{"467..114..", []int{}},
+		{"...*......", []int{3}},
+		{"..35..633.", []int{}},
+		{"......#...", []int{6}},
+		{"617*......", []int{3}},
+		{".....+.58.", []int{5}},
+		{"..592.....", []int{}},
+		{"......755.", []int{}},
+		{"...$.*....", []int{3, 5}},
+		{".664.598..", []int{}},
+	}
+
+	for _, test := range tests {
+		testName := fmt.Sprintf("%s = %v", test.input, test.expected)
+		t.Run(testName, func(t *testing.T) {
+			result := ExtractSymbolIndexesFromLine(test.input)
+			if !helpers.IntSlicesEqual(result, test.expected) {
+				t.Errorf("ExtractSymbolIndexesFromLine(%s) = %v; expected %v", test.input, result, test.expected)
+			}
+		})
+	}
+}
+
+func TestExtractPartNumbersFromLine(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected PartNumberSlice
+	}{
+		{"467..114..", PartNumberSlice{
+			{467, 0, 3},
+			{114, 5, 3},
+		}},
+		{"...*......", PartNumberSlice{}},
+		{"..35..633.", PartNumberSlice{
+			{35, 2, 2},
+			{633, 6, 3},
+		}},
+		{"......#...", PartNumberSlice{}},
+		{"617*......", PartNumberSlice{
+			{617, 0, 3},
+		}},
+		{".....+.58.", PartNumberSlice{
+			{58, 7, 2},
+		}},
+		{"..592.....", PartNumberSlice{
+			{592, 2, 3},
+		}},
+		{"......755.", PartNumberSlice{
+			{755, 6, 3},
+		}},
+		{"...$.*....", PartNumberSlice{}},
+		{".664.598..", PartNumberSlice{
+			{664, 1, 3},
+			{598, 5, 3},
+		}},
+	}
+
+	for _, test := range tests {
+		testName := fmt.Sprintf("%s = %v", test.input, test.expected)
+		t.Run(testName, func(t *testing.T) {
+			result := ExtractPartNumbersFromLine(test.input)
+			fmt.Printf("result = %v; expected = %v\n", result, test.expected)
+			if result.Equals(test.expected) {
+				t.Errorf("ExtractSymbolIndexesFromLine(%s) = %v; expected %v", test.input, result, test.expected)
+			}
+		})
+	}
 }
